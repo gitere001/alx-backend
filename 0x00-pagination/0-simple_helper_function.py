8a36a37d598d98fdbc5123b4a320d30ac86ec952
James gitere <sasmuypwa2019.com>
2024-07-20 11:55:01 +0300

    implimented simple pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,17 +2,17 @@
 """A module containing index_range"""
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """
     Calculate the start and end indices for a given page and page size.
 
-    Parameters:
+    Args:
         page (int): The page number.
         page_size (int): The number of items per page.
 
     Returns:
-        tuple: A tuple containing the start index and end index.
+        tuple: A tuple containing the start index (int) and end index (int).
     """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
     return (start_index, end_index)
