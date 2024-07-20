7399616dce57f1101fda10380c9c21def42e9863
James gitere <sasmuypwa2019.com>
2024-07-20 12:21:44 +0300

    implimented hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""a class to manage index range for pagination"""
+
+import csv
+import math
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get a specific page of the dataset based on the page number and page
+        size.
+
+        Args:
+            self: The Server instance.
+            page (int): The page number to retrieve (default is 1).
+            page_size (int): The number of items per page (default is 10).
+
+        Returns:
+            List[List]: A list of lists representing the subset of data for
+            the given page.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        try:
+            return dataset[start_index:end_index]
+        except IndexError:
+            return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Retrieves a hypermedia representation of a specific page of the
+        dataset based on the given page number and page size.
+
+        Args:
+            page (int, optional): The page number to retrieve. Defaults to 1.
+            page_size (int, optional): The number of items per page. Defaults
+            to 10.
+
+        Returns:
+            dict: A dictionary containing the hypermedia representation of the
+            page. The dictionary has the following keys:
+                - "page_size" (int): The number of items in the page.
+                - "Page" (int): The page number.
+                - "data" (list): A list of lists representing the subset of
+                data for the given page.
+                - "next_page" (int or None): The page number of the next page,
+                or None if there is no next page.
+                - "prev_page" (int or None): The page number of the previous
+                page, or None if there is no previous page.
+                - "total_page" (int): The total number of pages.
+        """
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+        return {
+            "page_size": len(data),
+            "Page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_page": total_pages
+        }
