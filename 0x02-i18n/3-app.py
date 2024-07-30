a88b0d65624b201620e2fc59c137092e1b32383e
James gitere <sasmuypwa2019.com>
2024-07-30 14:44:28 +0300

    added docstring

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+"""Flask configuration and application setup."""
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config(object):
-    """
-    Configuration for Babel
-    """
+    """Configuration class for Flask and Babel."""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
@@ -27,7 +19,11 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """
-    Select and return best language match based on supported languages
+    Returns the best matching locale from the list of available locales based
+    on the user's accepted languages.
+    This function is used as a localeselector for the Flask-Babel extension.
+
+    :return: A string representing the best matching locale.
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -35,10 +31,14 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Handles / route
+    A Flask route decorator that handles the root URL ("/") and renders the
+    "3-index.html" template.
+
+    Returns:
+        The rendered HTML content of the "3-index.html" template.
     """
     return render_template('3-index.html')
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(port=5000, host="0.0.0.0", debug=True)
