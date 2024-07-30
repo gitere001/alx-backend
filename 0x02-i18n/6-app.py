1ee544a3cff44d0f0004c2e3300c0800f505e36b
James gitere <sasmuypwa2019.com>
2024-07-30 17:38:18 +0300

    upddated

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+'''Task 4: Force locale with URL parameter.
+
+This module sets up a Flask application with Babel to handle
+localization. It includes user retrieval, locale selection, and
+routing for the main page.
+'''
+
+from typing import Dict, Union
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    '''Configuration class for the Flask app.
+
+    This class contains configuration settings for the Flask
+    application, including debug mode, supported languages,
+    default locale, and default timezone.
+    '''
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """Retrieve a user based on a user id.
+
+    Retrieves a user dictionary from the users list based on
+    the `login_as` query parameter.
+
+    Returns:
+        dict or None: The user dictionary if found, None otherwise.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Perform routines before each request's resolution.
+
+    Sets the global user object to the user retrieved from
+    the `get_user` function.
+    """
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Select and return the best language match based on supported languages.
+    """
+    # Locale from URL parameters
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+
+    # Locale from user settings
+    if g.user:
+        loc = g.user.get('locale')
+        if loc and loc in app.config['LANGUAGES']:
+            return loc
+
+    # Locale from request headers
+    loc = request.headers.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+
+    # Default to the best match from the request headers
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    '''Render the homepage.
+
+    Renders the homepage template.
+
+    Returns:
+        str: The rendered homepage template.
+    '''
+    return render_template("5-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
