f2eac5db684d49829527a8df1055705bbc6a7ca9
James gitere <sasmuypwa2019.com>
2024-07-30 16:11:31 +0300

    updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import Flask, render_template
+'''Task 0: Basic Flask app
+'''
 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('0-index.html')
+@app.route('/')
+def index():
+    '''default route'''
+    return render_template("0-index.html",)
 
 
 if __name__ == "__main__":
